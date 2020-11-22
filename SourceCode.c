749af0ce63703e1e5c02d3f86ee08e9f7d33a738
ziya <52481734+yoursweetie@users.noreply.github.com>
2020-11-22 13:41:23 +0000

    Update SourceCode.c

--- SourceCode.c
+++ SourceCode.c
@@ -15,12 +15,12 @@ unsigned char code  t[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F,
 
 void init_timer0(void) 	   //中断初始化函数
 {
-   	TMOD = 0x01;  /*定时器工作方式寄存器，工作在方式1*/ 
-   	TH0 = (65536 - 50000) / 256;  
+    TMOD = 0x01;  /*定时器工作方式寄存器，工作在方式1*/ 
+    TH0 = (65536 - 50000) / 256;  
     TL0 = (65536 - 50000) % 256;  
     EA = 1;   /*中断允许寄存器IE(8位)中的中断总控制位，EA=1时，CPU开放所有中断*/  
     ET0 = 1;  /*IE寄存器中的一位,定时/计数器T0中断控制位，ET0=1时,允许T0中断*/  
-   	TR0 = 1;  /*TR0：T0运行控制位，TR0=0,启动定时器T0*/ 
+    TR0 = 1;  /*TR0：T0运行控制位，TR0=0,启动定时器T0*/ 
 } 
 
 void timer(void) interrupt 1  //中断服务函数
@@ -28,7 +28,8 @@ void timer(void) interrupt 1  //中断服务函数
     TH0=(65536 - 50000) / 256;
     TL0=(65536 - 50000) % 256;
     f++;
-    if(f == 20)
+    
+    if (f == 20)
     {
         f = 0;
         m--;
@@ -47,26 +48,27 @@ void display(unsigned char time)    //控制两个数码管的显示倒计时函
     P2 = t[gewei];
 }
 
-void main() 
+void main()    //主函数
 {
     m = nanbei_time;
-   	P1 = 0x00;
+    P1 = 0x00;
     init_timer0();
-   	while(1) 
+    
+    while (1) 
     {
-  	    do
+        do
         {
-	        display(m);
-   	        g1=1; 
+	    display(m);
+   	    g1=1; 
             r1=0;
             g2=0;
             r2=1;
         }
-        while(m!=3);
+        while (m != 3);
 
         do
         {        
-            if(m<=3)
+            if (m <= 3)
             {
                 y1 = ~y1;    //取反，y1输出0变为1，黄灯亮起
                 r1 = 0;
@@ -79,54 +81,54 @@ void main()
             g2 = 0;
             r2 = 1;
         }
-        while(m != 3);
+        while (m != 3);
 
         do
         {        
-            if(m <= 3)
+            if (m <= 3)
             {
                 y1 = ~y1;
                 r1 = 0;
 		g1 = 0;
                 r2 = 1;
 		g2 = 0; 
-             }
+            }
             display(m);    
         }
-		while(m != 0);
+	while (m != 0);
 
-		if(m == 0)
+	if (m == 0)
         {
             m = dongxi_time;
-		    y1 = 0;
-		    y2 = 0;
+	    y1 = 0;
+	    y2 = 0;
         }
 
-	    do
+	do
         {
-	        display(m);
-	        g1 = 0;
-	        r1 = 1;
-	        g2 = 1;
-	        r2 = 0;
-	    }
-	    while(m != 3);
+	    display(m);
+	    g1 = 0;
+	    r1 = 1;
+	    g2 = 1;
+	    r2 = 0;
+	}
+	while (m != 3);
 
-	    do
+	do
         {
-	        if(m <= 3)
+	    if(m <= 3)
             {    
                 r1 = 1;
 		g1 = 0;
                 y2 = ~y2;
 		r2 = 0;
 		g2 = 0;
-            }
-            display(m);
-        }
-        while(m != 0);
+             }
+             display(m);
+	}
+        while (m != 0);
 
-        if(m == 0)
+        if (m == 0)
         {
             m = nanbei_time;
             y1 = 0;
